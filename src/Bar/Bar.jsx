8b75fca97ac105c292c71a853bdc79d51af5dec7
KevinLohJunYong <e0439232@u.nus.edu>
2021-06-02 15:09:52 +0800

    trying to make more mobile friendly

--- src/Bar/Bar.jsx
+++ src/Bar/Bar.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+const widthAdjusted = window.screen.availWidth < 500 ? "20px" : "30px";
+
 export default class Bar extends React.Component {
      getBgColor(isBeingSorted) {
         if(isBeingSorted) return "red";
         return "turquoise";
+     }
+     adjustHeight(height) {
+       if(window.screen.availWidth < 500) return height/2;
+       return height;
      }
      render() {
          const {
@@ -14,7 +20,7 @@ export default class Bar extends React.Component {
          return (
             <div 
               id={index}
-              style={{height:`${height}px`,width:"30px",backgroundColor:this.getBgColor(isBeingSorted),display:"inline-block",margin:"1px",marginTop:"10%"}}>
+              style={{height:`${this.adjustHeight(height)}px`,width:widthAdjusted,backgroundColor:this.getBgColor(isBeingSorted),display:"inline-block",margin:"1px",marginTop:"10%"}}>
             </div>
          );
      };
